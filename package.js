4b5b475dadd9384eae7639d96d4c3b312445a8ea
dominicthwaites <dominicthwaites@mac.com>
2024-11-08 18:07:02 +0000

    Package updates

--- package.js
+++ package.js
@@ -5,16 +5,16 @@ Package.describe({
   git: "https://github.com/gwendall/meteor-autoform-i18n.git"
 });
 
-var packages = [
-  'ecmascript',
-  "aldeed:simple-schema@1.3.2 || 2.0.0",
-  "tap:i18n@1.4.1 || 2.0.1",
-  "templating@1.1.1",
-  "underscore@1.0.3"
-];
-
 Package.onUse(function(api, where) {
+  const packages = [
+    'ecmascript',
+    "aldeed:simple-schema@1.3.2 || 2.0.0",
+    "tap:i18n@1.4.1 || 2.0.1",
+    "templating@1.1.1",
+    "underscore@1.0.3"
+  ];
   api.versionsFrom(['2.3', '3.0']);
   api.use(packages);
-  api.addFiles(["package-tap.i18n", "shared/lib.js"]);
+  api.addFiles(["package-tap.i18n"]);
+  api.mainModule("shared/lib.js");
 });
